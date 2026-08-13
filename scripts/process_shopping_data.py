1777f4ed5f1fc38a8bcfe8a4017bc4267c21cf2e
AlexandreBandelier <alexandre.bandelier@gmail.com>
2026-08-13 10:25:58 +0100

    Update process_shopping_data.py

--- scripts/process_shopping_data.py
+++ scripts/process_shopping_data.py
@@ -1,4 +1,1 @@
-from google.colab import drive
-drive.mount('/content/drive')
-df_ads = pd.read_csv('/content/drive/My Drive/Data_Shopping/export_google_ads.csv')
-df_feed = pd.read_xml('/content/drive/My Drive/Data_Shopping/feed_shopping_daily.xml')
+
